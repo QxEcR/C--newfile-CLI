213d5e25395c0554cc9a49675d07552173ccbb39
QxEcR <qxecrx1110@gmail.com>
2021-03-24 10:52:21 +0300

    exit if no file name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'cpp-newfile-cli',
-    version = '0.1.0',
+    version = '0.1.1',
     packages = ['maincli'],
     entry_points = {
         'console_scripts': [
