8dafad0dee9b9a0cf8c4d5b50e03fc58d8296d33
QxEcR <qxecrx1110@gmail.com>
2021-03-24 03:31:30 +0300

    update

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import  os
 
 os.system("touch main.cpp")
 with open('main.cpp',"w+") as f:
-    f.write("#include <iostream>\n")
-    f.write("#include <string>\n\n")
-    f.write("using namespace std;\n\n")
-    f.write("int main()\n{\n")
-    f.write("   /* write your code here */\n")
-    f.write("   return 0;\n")
-    f.write("}")
+    f.write("""
+            #include <iostream>\n
+            #include <string>\n\n
+            using namespace std;\n\n
+            int main()\n{\n
+               /* write your code here */\n
+               return 0;\n
+            }
+    """)
